2877c03c285e008944bcaecce45970c9bf0478e5
emilywang0 <emilywang0416@gmail.com>
2023-06-10 23:40:54 -0700

    "nonvuln version"

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# MergeBase 06/12/23: Vulnerable w/ Gradle
-## Vulnerable Versions Tested Using Gradle
-Below are the Java libraries and versions used in this test application and which CVE's they are vulnerable to.
+# MergeBase 06/12/23: NON-Vulnerable w/ Gradle
+## Non-Vulnerable Versions Tested Using Gradle
+Below are the Java libraries and versions used in this test application. They are NOT vulnerable to ANY of the
+CVE's mentioned in the vulnerable version. All versions are the latest as of 06/12/23, EXCEPT for Apache Tomcat
+and Spring Framework since older versions are required for the code in Main to compile.
 View <i>build.gradle</i> for all dependencies.
 
-| Java Library + Version | CVE        |
-|------------------------|------------|
-| Log4j 2.15.0           | CVE-2021–44228<br/>CVE-2019-17571 |
-| Apache Tomcat 9.0.30         | CVE-2020-1938 <br/>CVE-2020-17527<br/>CVE-2020-13935 |
-| Scala Library 2.13.8          | CVE-2022-36944 |
-| Spring Framework 5.2.8         | CVE-2020-5421 |
-| Apache Maven 3.3.1           | CVE-2022-29599<br/>CVE-2021-26291 |
-| Logback 1.2.7         | CVE-2021-42550 |
+| Java Library + Version   | 
+|--------------------------|
+| Log4j 2.20.0             |
+| Apache Tomcat 9.0.54     |  
+| Scala Library 2.13.11-M2 |     
+| Spring Framework 5.3.8   |   
+| Apache Maven 3.9.0       | 
+| Logback 1.4.7            |
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,26 @@
 package org.example;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
-import scala.collection.mutable.ListBuffer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.slf4j.LoggerFactory;
+import scala.collection.mutable.ListBuffer;
 
 import javax.servlet.ServletException;
 
 
-// MergeBase: Vulnerable Versions of Java Libraries for GRADLE
+// MergeBase: NON-Vulnerable Versions of Java Libraries for GRADLE
 public class Main {
     public static void main(String[] args) throws ServletException {
-        // Uses Log4j 2.15.0
+        // Uses Log4j 2.20.0
         final Logger logger = LogManager.getLogger();
         logger.debug("This is a debug message");
         logger.info("This is an info message");
         logger.warn("This is a warning message");
         logger.error("This is an error message");
 
-        // Uses Apache Tomcat 9.0.30
+        // Uses Apache Tomcat 9.0.54
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
 
@@ -29,7 +28,7 @@ public class Main {
         Tomcat.addServlet(context, "helloServlet", new HelloServlet());
         context.addServletMappingDecoded("/", "helloServlet");
 
-        // Uses Scala Library 2.13.8
+        // Uses Scala Library 2.13.11-M2
         ListBuffer<Integer> list = new ListBuffer<>();
         list.$plus$eq(1);
         list.$plus$eq(2);
@@ -38,14 +37,14 @@ public class Main {
         list.$plus$eq(5);
         System.out.println(list.mkString(", "));
 
-        // Uses Spring Framework 5.2.8
+        // Uses Spring Framework 5.3.8
 
         try (AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(MyBean.class)) {
             MyBean myBean = appContext.getBean(MyBean.class);
             myBean.sayHello();
         }
 
-        // Program also uses Apache Maven 3.3.1 and Logback 1.2.7 but usages omitted
+        // Program also uses Apache Maven 3.9.0 and Logback 1.4.7 but usages omitted
     }
 
 
